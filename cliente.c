f4db5b534da073cc8659fd06d167fed316f1de48
Felipe <lipemateus_boaventura@hotmail.com>
2019-04-03 17:41:27 -0300

    conexão simples entre cliente e servidor

--- cliente.c
+++ cliente.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+
+
 
 
 int main(){
@@ -8,5 +17,19 @@ int main(){
     if(sockfd<0){
         printf("error");
     }
+    char buffer[256];
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    server = gethostbyname("localhost");
+    bzero((char *) &serv_addr,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    serv_addr.sin_port = htons(5000);
+
+    if(connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr))<0){
+        printf("Erro na connexão\n");
+    }
+
     printf("hello\n");
 }
